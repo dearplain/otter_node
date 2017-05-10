6023a6d0e5e139c5363a74fbda7c88f4b3125fad
luwencong <lw6c@qq.com>
2017-05-10 09:36:50 +0800

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+## Usage
+
+```
+docker run -d --restart=always --net=host --name otter_node -e MANAGER=ip:port -e NID=1 otter_node
+```
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 echo $NID > /node/conf/nid
-sed -ri 's/(otter.manager.address = ).*/\1 "'"$MANAGER"'"/' /node/conf/otter.properties
+sed -ri "s/(otter.manager.address).*/\1 = $MANAGER/" /node/conf/otter.properties
 /node/bin/startup.sh
+trap 'echo stop node; cd /node/bin; ./stop.sh' TERM
+tail -f /dev/null & wait
