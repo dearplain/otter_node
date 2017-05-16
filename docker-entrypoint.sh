aba93b14c30cc189ecc4162f15cb887b9acb1622
luwencong <lw6c@qq.com>
2017-05-16 14:33:09 +0800

    stop node first; add MaxMem parameter

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 echo $NID > /node/conf/nid
+echo NID: $NID
+echo MaxMem: $MaxMem
+if [ -z "$MaxMem" ];then
+	MaxMem=3072m
+else
+    sed -i s/"-server -Xms32m -Xmx3072m"/"-server -Xms32m -Xmx$MaxMem"/g /node/bin/startup.sh
+fi
 sed -ri "s/(otter.manager.address).*/\1 = $MANAGER/" /node/conf/otter.properties
+/node/bin/stop.sh
 /node/bin/startup.sh
-trap 'echo stop node; cd /node/bin; ./stop.sh' TERM
-tail -f /dev/null & wait
+trap 'echo stop node; /node/bin/stop.sh' TERM
+# tail -f /dev/null & wait
+sleep 2
+tail -f /node/logs/node/node.log & wait
